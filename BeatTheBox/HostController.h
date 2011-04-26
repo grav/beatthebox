757012db563d814de7328257ade091b4172a236c
Mikkel Gravgaard <grav@meshell.(none)>
2011-04-26 21:04:25 +0200

    interface and mock for hostcontroller

---
+++ BeatTheBox/HostController.h
@@ -0,0 +1,22 @@
+//
+//  HostController.h
+//  BeatTheBox
+//
+//  Created by Mikkel Gravgaard on 26/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+class IHostController{
+public: 
+    virtual ~IHostController(){}
+    virtual void makeOnset() = 0;
+    virtual void restartLoop() = 0;
+};
+
+class HostControllerMock : public IHostController {
+public:
+    virtual void makeOnset(){}
+    virtual void restartLoop(){}
+    
+};
+
+
---
+++ UnitTests/SegmentTest.cpp
@@ -0,0 +1,9 @@
+//
+//  SegmentTest.cpp
+//  BeatTheBox
+//
+//  Created by Mikkel Gravgaard on 26/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include "SegmentTest.h"
