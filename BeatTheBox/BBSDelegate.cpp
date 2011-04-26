b97c25b742ddc43072d552c39d72b5bfd2bdf5d0
Mikkel Gravgaard <grav@meshell.(none)>
2011-04-26 21:25:11 +0200

    methods in implementation do not have to be virtual

---
+++ BeatTheBox/BBSDelegate.cpp
@@ -0,0 +1,9 @@
+//
+//  BBSDelegate.cpp
+//  BeatTheBox
+//
+//  Created by Mikkel Gravgaard on 26/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include "BBSDelegate.h"
---
+++ BeatTheBox/BBSDelegate.h
@@ -0,0 +1,7 @@
+//
+//  BBSDelegate.h
+//  BeatTheBox
+//
+//  Created by Mikkel Gravgaard on 26/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
