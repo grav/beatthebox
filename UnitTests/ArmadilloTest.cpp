d6b0fa2abf8dff7758f4c92d345cbbd8cbcd98df
Mikkel Gravgaard <grav@meshell.(none)>
2011-05-12 15:19:17 +0200

    almost copy paste :-)

---
+++ UnitTests/ArmadilloTest.cpp
@@ -0,0 +1,9 @@
+//
+//  ArmadilloTest.cpp
+//  BeatTheBox
+//
+//  Created by Mikkel Gravgaard on 12/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include "ArmadilloTest.h"
---
+++ UnitTests/BlitzTest.cpp
@@ -0,0 +1,36 @@
+//
+//  BlitzTest.cpp
+//  BeatTheBox
+//
+//  Created by Mikkel Gravgaard on 12/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include "gtest/gtest.h"
+#include <blitz/array.h>
+using namespace blitz;
+TEST(BlitzTest,SimpleExample){
+    Array<int,2> A(3,3), B(3,3), C(3,3), EXPECTED(3,3);
+    A = 
+    1, 0, 0,
+    2, 2, 2,
+    1, 0, 0;
+    
+    B = 
+    0, 0, 7,
+    0, 8, 0,
+    9, 9, 9;
+    
+    C = A + B;
+    
+    EXPECTED = 
+    1, 0, 7,
+    2, 10, 2,
+    10, 9, 9;
+
+    EXPECT_EQ(EXPECTED.data(), C.data());
+    
+    EXPECT_DOUBLE_EQ(EXPECTED(1,1),C(1,1));
+
+    
+}
