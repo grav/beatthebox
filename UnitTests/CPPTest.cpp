24a3b85d46bca9e692534dd5078267f6c9e2754e
Mikkel Gravgaard <mikkelg@gmail.com>
2011-05-14 18:42:58 +0200

    more stl testing

--- UnitTests/CPPTest.cpp
+++ UnitTests/CPPTest.cpp
@@ -8,7 +8,6 @@
 
 #include "gtest/gtest.h"
 #include <map.h>
-#include <string.h>
 #include "constants.h"
 
 using namespace std;
@@ -20,5 +19,12 @@ TEST(CPPTest, Map){
     EXPECT_EQ(BD, data["foo"]);
     data["bar"] = SD;
     EXPECT_EQ(SD, data["bar"]);
+}
+
+TEST(CPPTest,Vector){
+    double arr[] = {1,2,3};
+    int len=3;
+    vector<double> v;
+    v.assign(arr, arr+len);
 }
 
