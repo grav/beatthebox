d2a11910ee7878fc67cb49e1b12cb1f0a1c9598d
Mikkel Gravgaard <mikkelg@gmail.com>
2011-05-16 13:48:14 +0200

    woops?

--- BeatTheBox/LinearAlgebra.h
+++ BeatTheBox/LinearAlgebra.h
@@ -5,3 +5,18 @@
 //  Created by Mikkel Gravgaard on 16/05/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
+#include <vector.h>
+#include <math.h>
+#include <assert.h>
+#include "DSP.h"
+
+namespace linalg{
+    double l2norm(vector<double>* v);  
+    vector<double>* add(vector<double>* a, vector<double>* b);
+    vector<double>* times(vector<double> *a, double c);
+    double dot(vector<double>* a, vector<double>* b);
+    vector<double>* extendWithOne(vector<double> v);
+    vector<double>* randomUnitVector(int length);
+
+
+};
