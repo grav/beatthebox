9bb1bf530ce8ace91923a5498c88e7f99b714ed2
Mikkel Gravgaard <mikkelg@gmail.com>
2011-05-17 14:12:16 +0200

    test

--- UnitTests/SoundHelperTest.cpp
+++ UnitTests/SoundHelperTest.cpp
@@ -9,7 +9,7 @@
 #include "gtest/gtest.h"
 #include "SoundHelper.h"
 #include <iostream.h>
-
+#include "DSP.h"
 TEST(SoundHelperTest,LoadMono){
     std::string fileName = "/Users/grav/repositories/uni/feature/session2/mikkel_02.wav";
     
@@ -18,4 +18,13 @@ TEST(SoundHelperTest,LoadMono){
     SoundHelper::loadMono(fileName,samples,numSamples);
     EXPECT_EQ(122880,numSamples);
 
+    
+}
+
+TEST(SoundHelperTest,SaveMono){
+    std::string fileName = "/Users/grav/Desktop/test.wav";
+    int length = 44100;
+    double *noise = DSP::noise(length);
+    double *noiseNorm = DSP::map(noise, length, ^(double x){return x*.1-1;});
+    SoundHelper::saveMono(fileName, noiseNorm, length);
 }
