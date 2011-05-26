3e26630361a4986b6d0655b56034ebb831d90334
Mikkel Gravgaard <mikkelg@gmail.com>
2011-05-26 15:17:21 +0200

    template tests. yeah i know, don't test the std library ...

---
+++ UnitTests/TemplatesTest.cpp
@@ -0,0 +1,29 @@
+#include "gtest/gtest.h"
+#include <vector.h>
+
+#define VEC vector<T>
+
+using namespace std;
+
+template <typename T>
+T foo(T a){
+    return a+1;
+}
+
+template <typename T>
+T bar(VEC v){
+    return v[0]+10;
+}
+
+TEST(Generics, Foo){
+    float f = 1;
+    EXPECT_EQ(2,foo(f));
+    double d = 1;
+    EXPECT_EQ(2,foo(d));
+    
+    double a[] = {1,2,3};
+    vector<double> v(a,a+3);
+    EXPECT_EQ(11, bar(v));
+    
+}
+
