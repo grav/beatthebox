2bbc6f92f3b342ec2de157377c287c1d9eaa4360
Mikkel Gravgaard <mikkel@klokke.dk>
2011-06-24 16:42:58 +0200

    extra test

--- UnitTests/LinearAlgebraTests.cpp
+++ UnitTests/LinearAlgebraTests.cpp
@@ -17,9 +17,10 @@ TEST(LinAlg,add){
     double b[] = {0.5, 0.7, -1, -11};
     vector<double> v2(b,b+length);
     double e[] = {1.5, 2.7, 2, -7};
-    vector<double> *r = add(&v1,&v2);
+    vector<double> r;
+    add(&v1,&v2,&r);
     for(int i=0;i<length;i++){
-        EXPECT_EQ(e[i],(*r)[i]); 
+        EXPECT_EQ(e[i],r[i]); 
     }
 }
 
@@ -29,9 +30,10 @@ TEST(LinAlg,times){
     vector<double> v1(a,a+length);
     double c = 2.5;
     double e[] = {2.3*2.5, 1.0*2.5, -3*2.5, 99*2.5};
-    vector<double> *r = times(&v1,c);
+    vector<double> r;
+    times(&v1,c,&r);
     for(int i=0;i<length;i++){
-        EXPECT_EQ(e[i],(*r)[i]); 
+        EXPECT_EQ(e[i],r[i]); 
     }
     
 }
@@ -53,11 +55,21 @@ TEST(LinAlg, extendWithOne){
     vector<double> v(a,a+length);
     double e[] = {2.3,1.0,-3,99,1};
     vector<double> ve(e,e+length+1);
-    vector<double> *r = extendWithOne(v);
+    vector<double> r;
+    extendWithOne(&v,&r);
     EXPECT_EQ(ve.size(),v.size()+1);
     for(int i=0;i<length;i++){
-        EXPECT_EQ((*r)[i], ve[i]);
+        EXPECT_EQ(r[i], ve[i]);
     }
-    EXPECT_EQ(r->back(), 1);
+    EXPECT_EQ(r.back(), 1);
     
+}
+
+TEST(LinAlg, randomUnitVector){
+    vector<double> v;
+    int n=10;
+    randomUnitVector(n,&v);
+    EXPECT_EQ(n,v.size());
+    int precision = 1000;
+    EXPECT_EQ(precision, precision*(int)(l2norm(&v)));
 }
